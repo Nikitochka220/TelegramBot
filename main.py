91717472e604ca095cf6a731968dcce3360fb51e
Nikitochka220 <sasha2011like@mail.ru>
2025-08-03 08:38:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+from telegram.ext import ApplicationBuilder, CommandHandler
 import os
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 async def start(update, context):
-    await update.message.reply_text("Бот работает!")
+    await update.message.reply_text("✅ Бот запущен и работает!")
 
 app = ApplicationBuilder().token(os.getenv("TOKEN")).build()
 app.add_handler(CommandHandler("start", start))
-app.run_polling()
+
+app.run_polling()  # <=== ОБЯЗАТЕЛЬНО! Не удаляй!
